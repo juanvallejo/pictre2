7cef6169a5704a9879fd220a252201e2e682d478
juanvallejo <juan.vallejo.12@cnu.edu>
2016-08-15 00:45:49 -0400

    add support for node resize events
    Also adds support for node state and node property value caching
    for the events module.

--- src/events.js
+++ src/events.js
@@ -6,6 +6,8 @@ var Events = {};
 var registeredGlobalEvents = {};
 var registeredNodeEvents = {};
 
+var nodeStateCache = {};
+
 /**
  * Listens for a dom event
  */
@@ -41,6 +43,87 @@ Events.onNodeEvent = function(node, eventName, callback) {
 	} catch (e) {
 		node.attachEvent('on' + eventName, callback);
 	}
+};
+
+/**
+ * Calls callback function scoped to node passed
+ * with the 'resize' event as the first parameter
+ * the horizontal difference as the second parameter
+ * and the vertical difference as the third parameter
+ */
+Events.onNodeResizeEvent = function(node, callback) {
+	// nodeStateCache can be used by any method on this module
+	// therefore, delay any instantiation until after we know
+	// an entry for this node exists
+	if (!nodeStateCache[node.nodeName]) {
+		nodeStateCache[node.nodeName] = {};
+	}
+	if (!nodeStateCache[node.nodeName].onNodeResizeEvent) {
+		nodeStateCache[node.nodeName].onNodeResizeEvent = true;
+		nodeStateCache[node.nodeName].registeredResizeCallbacks = [];
+		nodeStateCache[node.nodeName].lastWidth = node.innerWidth || node.clientWidth;
+		nodeStateCache[node.nodeName].lastHeight = node.innerHeight || node.clientHeight;
+	} else {
+		nodeStateCache[node.nodeName].registeredResizeCallbacks.push(callback);
+		return;
+	}
+
+	nodeStateCache[node.nodeName].registeredResizeCallbacks.push(callback);
+
+	try {
+		node.addEventListener('resize', eventHandler);
+	} catch (e) {
+		node.attachEvent('onresize', eventHandler);
+	}
+
+	function eventHandler(e) {
+		return (function(node) {
+			var width = node.innerWidth;
+			var height = node.innerHeight;
+
+			if (!width) {
+				width = node.clientWidth;
+			}
+			if (!height) {
+				height = node.clientHeight;
+			}
+
+			var horDiff = null;
+			var vertDiff = null;
+			if (width != nodeStateCache[node.nodeName].lastWidth) {
+				horDiff = (width - nodeStateCache[node.nodeName].lastWidth || 0);
+			}
+			if (height != nodeStateCache[node.nodeName].lastHeight) {
+				vertDiff = (height - nodeStateCache[node.nodeName].lastHeight || 0);
+			}
+
+			// iterate through all registered resize events for this node
+			for (var i = 0; i < nodeStateCache[node.nodeName].registeredResizeCallbacks.length; i++) {
+				if (typeof nodeStateCache[node.nodeName].registeredResizeCallbacks[i] == 'function') {
+					nodeStateCache[node.nodeName].registeredResizeCallbacks[i].call(node, e, horDiff, vertDiff);
+				}
+			}
+
+			nodeStateCache[node.nodeName].lastWidth = width;
+			nodeStateCache[node.nodeName].lastHeight = height;
+		})(node);
+	}
+}
+
+Events.onNodeHorizontalResizeEvent = function(node, callback) {
+	Events.onNodeResizeEvent(node, function(e, horizontalDiff, verticalDiff) {
+		if (horizontalDiff) {
+			callback.call(node, horizontalDiff);
+		}
+	});
+};
+
+Events.onNodeVerticalResizeEvent = function(node, callback) {
+	Events.onNodeResizeEvent(node, function(e, horizontalDiff, verticalDiff) {
+		if (verticalDiff) {
+			callback.call(node, verticalDiff);
+		}
+	});
 };
 
 // executes event "callbacks" on a node event and stores them
